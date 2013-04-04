25fc322251df081075b00b55a54610f0acb07fe5
dsantiago89 <dsantiago@telegestion.com>
2013-04-04 17:51:15 +0200

    Initial commit

---
+++ pb_privilegios/WebFolder/index.waPage/scripts/index-tablet.js
@@ -0,0 +1,36 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		
+    var emp = ds.Usuarios.newEntity(); // create the entity
+    var usuario=textField1;
+    emp.user.setValue(usuario); // assigning storage attributes
+    var password=textField2
+    emp.pwd.setValue(password);
+    var grupo=select1;
+    emp.Id_grupo.setValue(grupo);
+   
+    emp.save({
+        onSuccess:function(event)
+        {
+            $("#display").html("saved ok"); // display the result in a Container widget
+        },
+            onError:function(event)
+        {
+            $("#display").html("error on save");
+        }
+    });
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
